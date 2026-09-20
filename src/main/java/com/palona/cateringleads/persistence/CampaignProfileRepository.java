d64080b6945f3c72e3cdf2d3a2f41d0532b74af6
KevinHaolinChen <54457697+KevinHaolinChen@users.noreply.github.com>
2026-09-20 08:00:21 -0700

    Add campaign profile repository

---
+++ src/main/java/com/palona/cateringleads/persistence/CampaignProfileRepository.java
@@ -0,0 +1,5 @@
+package com.palona.cateringleads.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignProfileRepository extends JpaRepository<CampaignProfileEntity, String> {}
