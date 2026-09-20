ef87ed9061330ef6abd61629145178872ea3db1f
KevinHaolinChen <54457697+KevinHaolinChen@users.noreply.github.com>
2026-09-20 08:13:56 -0700

    Remove abandoned v0.8 scaffold

--- src/main/java/com/palona/cateringleads/model/ProspectSignalsResponse.java
+++
@@ -1,3 +0,0 @@
-package com.palona.cateringleads.model;
-
-public record ProspectSignalsResponse(String prospectId, int baseScore, int profileFit, int publicSignals, int adjustedScore, String summary, String sourceUrl) {}
