a1d8bb0b0b9187d9893fa21de0ee15997ad837c4
KevinHaolinChen <54457697+KevinHaolinChen@users.noreply.github.com>
2026-09-20 08:13:48 -0700

    Remove abandoned v0.8 scaffold

--- src/main/java/com/palona/cateringleads/persistence/CampaignProfileRepository.java
+++
@@ -1,5 +0,0 @@
-package com.palona.cateringleads.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignProfileRepository extends JpaRepository<CampaignProfileEntity, String> {}
