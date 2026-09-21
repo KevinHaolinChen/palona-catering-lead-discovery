4a764c206fb6cd4de8c9a52516b8c27cfb645c79
KevinHaolinChen <54457697+KevinHaolinChen@users.noreply.github.com>
2026-09-20 17:55:27 -0700

    Enable recurring Radius Watch scheduling

--- src/main/java/com/palona/cateringleads/CateringLeadApplication.java
+++ src/main/java/com/palona/cateringleads/CateringLeadApplication.java
@@ -2,7 +2,9 @@ package com.palona.cateringleads;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class CateringLeadApplication {
     public static void main(String[] args) { SpringApplication.run(CateringLeadApplication.class, args); }
